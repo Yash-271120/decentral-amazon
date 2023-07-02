90f1f2c2d1d2653211eb2120b39af2d2e0ccc90c
Yash Patil <yashpatil.yp2711@gmail.com>
2023-07-02 14:14:34 +0530

    commit before deploying

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,35 @@ const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return hre.ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 async function main() {
+    const [deployer] = await hre.ethers.getSigners()
 
+    const Decentral_Amazon = await hre.ethers.getContractFactory("Decentral_Amazon");
+    const decentral_Amazon = await Decentral_Amazon.deploy()
+    await decentral_Amazon.deployed();
+
+    console.log(`Deployed Decentral_Amazon Contract at: ${decentral_Amazon.address}`)
+
+    // List items...
+
+    for(let i = 0;i<items.length; i++) {
+      const transaction = await decentral_Amazon.connect(deployer).list(
+        items[i].id,
+        items[i].name,
+        items[i].category,
+        items[i].image,
+        tokens(items[i].price),
+        items[i].rating,
+        items[i].stock
+      )
+
+      await transaction.wait()
+
+      console.log(`Listed item ${items[i].id}: ${items[i].name}`);
+    }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Section from './components/Section'
-import Product from './components/Product'
+import Navigation from "./components/Navigation";
+import Section from "./components/Section";
+import Product from "./components/Product";
 
 // ABIs
-import Dappazon from './abis/Dappazon.json'
+import Decentral_Amazon from "./abis/Decentral_Amazon.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [decentralAmazon, setDecentralAmazon] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const togglePop = (item) => {
+    setItem(item);
+    setToggle(!toggle);
+  };
+
+  const loadBlockChainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network = await provider.getNetwork();
+
+    // Connect to smart contracts
+    const dappazon = new ethers.Contract(
+      config[network.chainId].decentral_amazon.address,
+      Decentral_Amazon.abi,
+      provider
+    );
+
+    setDecentralAmazon(dappazon);
+
+    // Load products
+
+    const items = [];
+
+    for (let i = 0; i < 9; i++) {
+      const item = await dappazon.items(i + 1);
+      items.push(item);
+    }
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockChainData();
+  }, []);
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Decentral Amazon's Best Sellers</h2>
+      {electronics && toys && clothing && (
+        <>
+          <Section
+            title={"Clothing & Jewelry"}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={"Electronics & Gadgets"}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
 
-      <h2>Welcome to Dappazon</h2>
-
+      {toggle && (
+        <Product item={item} provider={provider} decentralAmazon={decentralAmazon} togglePop={togglePop} account={account} />
+      )}
     </div>
   );
 }
